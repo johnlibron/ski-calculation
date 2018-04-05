e33381153fab5be1fb6cff7d683a3d502f376708
John <John@John>
2018-04-05 22:41:20 +0800

    Display 4x4 Map

--- src/com/ski/calculation/SkiCalculation.java
+++ src/com/ski/calculation/SkiCalculation.java
@@ -2,6 +2,21 @@ package com.ski.calculation;
 
 public class SkiCalculation {
 	public static void main(String[] args) {
-		System.out.println("Hello, World.");
+		
+		int[][] map = new int[][]{
+			{ 4, 8, 7, 3 },
+			{ 2, 5, 9, 3 },
+			{ 6, 3, 2, 5 },
+			{ 4, 4, 1, 6 }
+		};
+		
+		for (int row = 0; row < map.length; row++) {
+			System.out.print(row + " | ");
+			for (int col = 0; col < map[row].length; col++) {
+				System.out.print(map[row][col] + " ");
+			}
+			System.out.println();
+		}
+		
 	}
 }
